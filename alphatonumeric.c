933c1c3b7611bc811e8145b852aeb02c6bdb912e
Jordi Hernandez <joanhern@ucsc.edu>
2021-12-19 22:02:27 -0800

    completed ch7 programming project 4 K.N. King

--- alphatonumeric.c
+++ alphatonumeric.c
@@ -3,14 +3,28 @@
 
 int main(void){
   char ch;
-  printf("Enter phone number:");
+  printf("Enter phone number: ");
   do {
     ch = getchar();
     if (ch == 'A' || ch == 'B' || ch == 'C'){
       putchar('2');
+    }else if (ch == 'D' || ch == 'E' || ch == 'F'){
+      putchar('3');
+    }else if (ch == 'G' || ch == 'H' || ch == 'I'){
+      putchar('4');
+    }else if (ch == 'J' || ch == 'K' || ch == 'L'){
+      putchar('5');
+    }else if (ch == 'M' || ch == 'N' || ch == 'O'){
+      putchar('6');
+    }else if (ch == 'P' || ch == 'R' || ch == 'S'){
+      putchar('7');
+    }else if (ch == 'T' || ch == 'U' || ch == 'V'){
+      putchar('8');
+    }else if (ch == 'W' || ch == 'X' || ch == 'Y'){
+      putchar('9');
     }else{
       putchar(ch);
     }
-  }while (ch != '\n');
+  }while (ch != '\n' && ch != ' ');
   return 0;
 }
