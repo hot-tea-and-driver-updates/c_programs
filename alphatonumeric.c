ddb0c002e297078b901287aef0368f2ae4274467
Jordi Hernandez <joanhern@ucsc.edu>
2021-12-19 21:20:55 -0800

    90% done for program alphatonumeric

--- alphatonumeric.c
+++ alphatonumeric.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+//This program translates an alphabetic phone number into numeric form
+
+int main(void){
+  char ch;
+  printf("Enter phone number:");
+  do {
+    ch = getchar();
+    if (ch == 'A' || ch == 'B' || ch == 'C'){
+      putchar('2');
+    }else{
+      putchar(ch);
+    }
+  }while (ch != '\n');
+  return 0;
+}
